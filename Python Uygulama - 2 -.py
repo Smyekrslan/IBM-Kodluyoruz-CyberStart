1aa6e9f89af33e8c8b4e4e0e861698cc89b2b428
sümeyye <159816072+Smyekrslan@users.noreply.github.com>
2024-08-18 10:35:01 +0300

    Update Python Uygulama - 2 -.py

--- Python Uygulama - 2 -.py
+++ Python Uygulama - 2 -.py
@@ -1,14 +1,14 @@
 
 points = [(1, 2), (4, 6), (5, 8), (3, 7)]
 
-#Öklid Formülü
+
 def euclideanDistance(point1, point2):
     x1, y1 = point1
     x2, y2 = point2
     distance = ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5
     return distance
 
-# Mesafelerin hesaplanması
+
 distances = []
 for i in range(len(points)):
     for j in range(i + 1, len(points)):
